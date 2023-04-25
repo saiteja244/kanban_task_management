cd03be02da0fe06631fe37508370cf33f02da4fc
Oliver Cadman <o.cadman@live.co.uk>
2023-04-25 17:49:39 +0100

    Added utility map for padding and margins

--- app/src/components/Sidebar/Sidebar.jsx
+++ app/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ReactComponent as Logo } from "../../assets/svgs/logo-dark.svg";
 
 const Sidebar = () => {
-  return <aside class="sidebar"></aside>;
+  return (
+    <aside className="sidebar p-2">
+      <Logo className="logo logo-dark" />
+    </aside>
+  );
 };
 
 export default Sidebar;
